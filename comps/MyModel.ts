e31e683a5b04ddef896743e21402a19e1c5d0f01
rxlabz <dev@rxlabz.com>
2015-05-16 14:18:11 +0200

    remove old /comps pkg

--- comps/MyModel.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../main.ts" />
-
-export class MyModel{ 
-	items :Array<string>	
-	constructor(){
-		console.log("MyModel constructor");
-		this.items = [ "a" , "b" , "c" ];
-	}
-}
