572a53ac600d774534926ca8b9458a201837087e
rxlabz <dev@rxlabz.com>
2016-01-27 00:04:15 +0100

    + Tooltiper integration

--- app/components/itemRenderer.ts
+++ app/components/itemRenderer.ts
@@ -1,23 +1,32 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
-import { Component, EventEmitter} from "angular2/core";
+import { Component, EventEmitter, OnDestroy} from "angular2/core";
 
 import {Todo} from "../data/Todo"
 import {FormBuilder, Validators, FORM_BINDINGS} from "angular2/common";
 import {ControlGroup} from "angular2/common";
+import {ToolTiper} from "../directives/tooltiper.directive";
+import {StructuralTip} from "../../../../../../notes/NG2Notes/protos/structural-tooltip.directive.ts";
 
 @Component({
     selector: 'item-renderer',
     properties: ["item: item"],
-    events: ['deleteItem', 'updateItem'],
+    outputs: ['deleteItem', 'updateItem'],
+    directives: [ToolTiper],
     templateUrl: "app/components/ItemRenderer.html"
 })
-export class ItemRenderer{
+export class ItemRenderer implements OnDestroy {
 
     hasError:boolean;
 
     private _item:Todo;
     private todoForm:ControlGroup;
+
+    deleteItem:EventEmitter<any> = new EventEmitter();
+    updateItem:EventEmitter<any> = new EventEmitter();
+
+    editable:boolean;
+    lastValue:string;
 
     get item():Todo {
         return this._item;
@@ -26,27 +35,25 @@ export class ItemRenderer{
     set item(value:Todo) {
         this._item = value;
 
-        if (value) {
-            if (!this.todoForm) {
-                var fb = new FormBuilder()
-                this.todoForm = fb.group({
-                    title: [this._item.title, Validators.required]
-                });
-            } else {
-                this.todoForm.value.title = this._item.title;
-            }
-        }
+        if (value)
+            this.initRenderer();
     }
-
-    deleteItem:EventEmitter<any> = new EventEmitter();
-    updateItem:EventEmitter<any> = new EventEmitter();
-
-    editable:boolean;
-    lastValue:string;
 
     constructor() {
         this.editable = false;
     }
+
+    private initRenderer() {
+        if (!this.todoForm) {
+            let fb = new FormBuilder();
+            this.todoForm = fb.group({
+                title: [this._item.title, Validators.required]
+            });
+        } else {
+            this.todoForm.value.title = this._item.title;
+        }
+    };
+
 
     removeItem(item:string) {
         this.deleteItem.emit(item);
@@ -54,7 +61,6 @@ export class ItemRenderer{
     }
 
     edit() {
-        //this.todoForm.value.title = this.item.title;
         this.editable = true;
         this.lastValue = this.item.title;
     }
@@ -76,7 +82,6 @@ export class ItemRenderer{
             return;
         }
 
-        console.log("new title");
         this.updateItem.emit(this.item);
         this.editable = false;
     }
@@ -93,6 +98,9 @@ export class ItemRenderer{
 
     onItemFocus() {
         console.log("FOCUS");
+    }
 
+    ngOnDestroy() {
+        console.log('ngOnDestroy', this._item);
     }
 }
